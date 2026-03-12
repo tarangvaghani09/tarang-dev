8c1e534dcd863c29c16432f93061b24f108ef6bf
Tarang Vaghani <tarangvaghani@gmail.com>
2026-03-12 22:15:52 +0530

    qq

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -138,7 +138,7 @@ export function Navbar() {
           >
             {theme === "dark" ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
           </motion.span>
-        </motion.button>
+        </motion.button>   
         
         <button 
           onClick={() => scrollTo('contact')}
